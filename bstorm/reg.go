093513729a03b7807d988f60eceb47d952cb78f7
Zhigang Chen <zhigangc@ip-10-17-21-243.ec2.internal>
2012-05-25 15:33:49 -0700

    some updates

--- bstorm/reg.go
+++ bstorm/reg.go
@@ -10,46 +10,74 @@ package main
 import re "rubex"
 import "time"
 import "regexp"
+import "runtime"
+import "os"
+import "strconv"
 
+var re1 []*regexp.Regexp
+var re2 []*re.Regexp
+const NUM = 100
+var STR = "abcdabc"
 
-func rubex_page() {
+func init() {
+	re1 = make([]*regexp.Regexp, NUM)
+	re2 = make([]*re.Regexp, NUM)
+	for i := 0; i < NUM; i ++ {
+		re1[i] = regexp.MustCompile("[a-c]*$")
+		re2[i] = re.MustCompile("[a-c]*$")
+	}
+	for i := 0; i < 10; i ++ {
+		STR += STR
+	}
+	println("len:", len(STR))
+}
+
+func rubex_page(index int) {
 	for i := 0; i < 100; i++ {
-		r := re.MustCompile("[a-c]*$")
-		r.MatchString("abcdabc")
-		r.Free()
+		r := re2[index]
+		r.MatchString(STR)
 	}
 }
 
-func regexp_page() {
+func regexp_page(index int) {
 	for i := 0; i < 100; i++ {
-		r := regexp.MustCompilePOSIX("[a-c]*$")
-		r.MatchString("abcdabc")
-		//r.Free()
+		r := re1[index]
+		r.MatchString(STR)
 	}
 }
 
-func render_pages(name string, fp  func(), num_routines, num_renders int) {
+func render_pages(name string, fp  func(int), num_routines, num_renders int) {
 	for i := 0; i < num_routines; i++ {
 		go func () {
+			t := time.Now()
 			for j := 0; j < num_renders; j++ {
-				t := time.Now()
-				fp()	
-				println(name + "-average: " + time.Since(t).String())
+				fp(i)	
 			}
+			println(name + "-average: ",  time.Since(t).Nanoseconds()/int64(num_renders*1000000), "ms")
 		}()
 	}
 }
 
 func main() {
-
-	num_routines := 40
-	num_renders := 100
+	cpu, _ := strconv.Atoi(os.Args[1])
+	lib := os.Args[2]
+	println("using CPUs:", cpu)
+	runtime.GOMAXPROCS(cpu)
+	num_routines := 90
+	num_renders := 20
 	
-//	render_pages("rubex", rubex_page, num_routines, num_renders)
-	render_pages("regexp", regexp_page, num_routines, num_renders)
+	if lib == "rubex" {
+		render_pages("rubex", rubex_page, num_routines, num_renders)
+	} else {
+		render_pages("regexp", regexp_page, num_routines, num_renders)
+	}
 
-	d, _ := time.ParseDuration("5m")
-	time.Sleep(d)
+	d, _ := time.ParseDuration("5s")
+	for i := 0; i < 100; i ++ {
+		println("goroutine:", runtime.NumGoroutine())
+		time.Sleep(d)
+		
+	}
 	println ("Done")
 }
 
